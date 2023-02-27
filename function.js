cbe9b6dacab2bd737f2f72d560a5832c4c11b754
Daniel rubio <info.danielrubio@gmail.com>
2023-02-26 22:28:35 -0300

    agregar mensajes de consola

---
+++ function.js
@@ -0,0 +1,46 @@
+/**botones superiores */
+
+
+document.getElementById('boton_datos').addEventListener('click', function() {
+    console.log("se dio click en el boton datos personales")
+});
+
+document.getElementById('boton_formacion').addEventListener('click', function() {
+    console.log("se dio click en el boton Formacion Academica")
+});
+
+document.getElementById('boton_experiencia').addEventListener('click', function() {
+    console.log("se dio click en el boton Experiencia Laboral")
+});
+
+document.getElementById('boton_idiomas').addEventListener('click', function() {
+    console.log("se dio click en el boton Idioma")
+});
+
+document.getElementById('boton_redes').addEventListener('click', function() {
+    console.log("se dio click en el boton Redes Sociales")
+});
+
+
+/**botones inferiores */
+
+
+document.getElementById('boton_facebook').addEventListener('click', function() {
+    console.log("se dio click en el boton de Facebook")
+});
+
+document.getElementById('boton_twiter').addEventListener('click', function() {
+    console.log("se dio click en el boton de Twiter")
+});
+
+document.getElementById('boton_twitch').addEventListener('click', function() {
+    console.log("se dio click en el boton de Twitch")
+});
+
+document.getElementById('boton_linkedin').addEventListener('click', function() {
+    console.log("se dio click en el boton de Linkedin")
+});
+
+document.getElementById('boton_github').addEventListener('click', function() {
+    console.log("se dio click en el boton de Github")
+});
